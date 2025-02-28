95b84646469394c3f155cec1b7261efd92df4569
jmottster <github@jasonmott.com>
2025-02-28 15:22:04 -0500

    Prep for release 0.1.3

--- newtons_blobs/globals.py
+++ newtons_blobs/globals.py
@@ -9,7 +9,7 @@ by Jason Mott, copyright 2024
 __author__ = "Jason Mott"
 __copyright__ = "Copyright 2024"
 __license__ = "GPL 3.0"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __maintainer__ = "Jason Mott"
 __email__ = "github@jasonmott.com"
 __status__ = "In Progress"
