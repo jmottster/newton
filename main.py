28a890841062da14072c0c3a3c97d78c6f1cd4ad
jmottster <github@jasonmott.com>
2025-04-11 12:08:59 -0400

    Prep for release 0.1.7

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #
 # nuitka-project: --company-name="Jason Mott"
 # nuitka-project: --product-name="Newton's Blobs"
-# nuitka-project: --product-version=0.1.6
+# nuitka-project: --product-version=0.1.7
 # nuitka-project: --file-version=0.0.1
 # nuitka-project: --file-description="A randomly generated solar system simulator with planets and moons"
 # nuitka-project: --copyright="GPL-3.0 license"
